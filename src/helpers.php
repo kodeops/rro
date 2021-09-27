ad36dec499c69d509caee2b2fb50b28b312e3424
Xavier Hernández <xavier@kodeops.io>
2021-09-27 15:22:39 +0200

    Check that function has not been declared yet

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,8 @@
 <?php
 use Illuminate\Support\Str;
+use kodeops\rro\rroException;
 
-if (!function_exists('success')) {
+if (! function_exists('success')) {
     function success($response_message = null) {
         return (new \kodeops\rro\RichResponseObject(
             true, 
@@ -9,9 +10,11 @@ if (!function_exists('success')) {
             Str::slug($response_message, '_')
         ))->build();
     }
+} else {
+    throw new rroException("“success” function has been already defined");
 }
 
-if (!function_exists('error')) {
+if (! function_exists('error')) {
     function error($response_message = null) {
         return (new \kodeops\rro\RichResponseObject(
             false, 
@@ -19,9 +22,11 @@ if (!function_exists('error')) {
             Str::slug($response_message, '_')
         ))->build();
     }
+} else {
+    throw new rroException("“error” function has been already defined");
 }
 
-if (!function_exists('rro')) {
+if (! function_exists('rro')) {
     function rro($response) {
         if (isset($response->response)) {
             $isSuccess = true;
@@ -47,10 +52,14 @@ if (!function_exists('rro')) {
             $status_code
         ))->build();
     }
+} else {
+    throw new rroException("“rro” function has been already defined");
 }
 
-if (!function_exists('is_rro')) {
+if (! function_exists('is_rro')) {
     function is_rro($rro) {
         return $rro instanceof \kodeops\rro\RichResponseObject;
    }
-}   
+} else {
+    throw new rroException("“is_rro” function has been already defined");
+}
